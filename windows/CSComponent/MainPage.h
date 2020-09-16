6df90274b3c80af6d0dec48f69d203a032f97ba5
mouhsine bakhich <mbakhick@css.edu.om>
2020-09-16 11:01:32 +0400

    add project

---
+++ windows/CSComponent/MainPage.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+
+namespace winrt::CSComponent::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::CSComponent::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
+
